936e4a32b6d9c6a647c21092878b14d10fa56567
Diane INGABIRE <127386345+Idiane05@users.noreply.github.com>
2025-08-05 21:17:42 +0200

    deployment

--- server/index.js
+++ server/index.js
@@ -54,6 +54,6 @@ app.put('/api/habits/:id', (req, res) => {
   res.json(habit);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
